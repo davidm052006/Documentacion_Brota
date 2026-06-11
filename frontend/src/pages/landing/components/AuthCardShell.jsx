b2518c32c6d8fcb264ff3cde125b05c1e3f7d02e
JulianMg20 <Machadojulian99@gmail.com>
2026-06-10 21:10:33 -0500

    Modificacion de Login 0.0

--- frontend/src/pages/landing/components/AuthCardShell.jsx
+++ frontend/src/pages/landing/components/AuthCardShell.jsx
@@ -2,13 +2,13 @@ import React from 'react';
 
 function AuthCardShell({ title, description, children }) {
   return (
-    <div className="bg-black/15 backdrop-blur-md p-10 rounded-2xl shadow-xl w-full max-w-sm flex flex-col gap-6 border border-white/30">
-      <h3 className="text-2xl font-bold text-center text-black">{title}</h3>
-      {description && (
-        <p className="text-center text-sm text-black/80 leading-relaxed">{description}</p>
-      )}
-      {children}
-    </div>
+<div className=" bg-amber-50 p-10 rounded-2xl shadow-xl w-full max-w-sm flex flex-col gap-6 border border-gray-200">
+  <h3 className="text-2xl font-bold text-center text-black">{title}</h3>
+  {description && (
+    <p className="text-center text-sm text-gray-700 leading-relaxed">{description}</p>
+  )}
+  {children}
+</div>
   );
 }
 
--- frontend/src/pages/landing/components/PreLoginNavbar.jsx
+++ frontend/src/pages/landing/components/PreLoginNavbar.jsx
@@ -1,17 +1,13 @@
 function PreLoginNavbar() {
-    return (
-        <nav className="w-full flex justify-between items-center px-10 py-6 absolute top-0 left-0">
-            <h2 className="text-2xl font-bold text-[black]">BROTA</h2>
-            <nav>
-                <ul className="flex gap-8 text-black font-medium ">
-                    <li><a href="#" className="hover:text-[var(--color-primary)] transition-colors">Inicio</a></li>
-                    <li><a href="#" className="hover:text-[var(--color-primary)] transition-colors">Sobre mí</a></li>
-                    <li><a href="#" className="hover:text-[var(--color-primary)] transition-colors">Contacto</a></li>
-                </ul>
-            </nav>
-        </nav>
-    )
+  return (
+    <nav className="w-full flex justify-start items-center px-10 py-6 absolute top-0 left-0">
+      <ul className="flex gap-8 text-black font-medium">
+        <li><a href="#" className="hover:text-[var(--color-primary)] transition-colors">Inicio</a></li>
+        <li><a href="#" className="hover:text-[var(--color-primary)] transition-colors">Sobre mí</a></li>
+        <li><a href="#" className="hover:text-[var(--color-primary)] transition-colors">Contacto</a></li>
+      </ul>
+    </nav>
+  )
 }
-
 
 export default PreLoginNavbar;
