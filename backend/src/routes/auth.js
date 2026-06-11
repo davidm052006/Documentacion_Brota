93228f4b2770e98df7483ef5594409beac220ed8
Brayan <brayanmoreno4567@gmail.com>
2026-06-10 23:30:20 -0500

    Cambios en el archivo authcontroller

--- backend/src/routes/auth.js
+++ backend/src/routes/auth.js
@@ -5,7 +5,7 @@ const router = express.Router();
 const {
   forgotPassword,
   resetPassword
-} = require('../controllers/autocontroller');
+} = require('../controllers/authcontroller');
 
 router.post(
   '/forgot-password',
