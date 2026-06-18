6fa8884c93cc37070be97a8c1eed40223d83af22
JulianMg20 <Machadojulian99@gmail.com>
2026-06-17 22:43:29 -0500

    se borrar botones de Login

--- frontend/src/pages/landing/components/LoginNavbar.jsx
+++ frontend/src/pages/landing/components/LoginNavbar.jsx
@@ -2,9 +2,7 @@ function LoginNavbar() {
   return (
     <nav className="w-full flex justify-start items-center px-10 py-6 absolute top-0 left-0">
       <ul className="flex gap-8 text-black font-medium">
-        <li><a href="#" className="hover:text-[var(--color-primary)] transition-colors">Inicio</a></li>
-        <li><a href="#" className="hover:text-[var(--color-primary)] transition-colors">Sobre mí</a></li>
-        <li><a href="#" className="hover:text-[var(--color-primary)] transition-colors">Contacto</a></li>
+      
       </ul>
     </nav>
   )
